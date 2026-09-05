db33872ebe0f30cff1cd5d06ad926ecbae556ca2
MUMSAREAWESOME <j.halliday2011@gmail.com>
2026-09-06 07:54:18 +1000

    Update setup.sh to include DNS rules and new package
    Added DNS configuration for CleanBrowsing and updated package installation.

--- setup.sh
+++ setup.sh
@@ -36,9 +36,18 @@ echo "Installing dependencies"
 
 yay -S wayle-bin
 
-sudo pacman -S nautilus firefox fuzzel hyprlock starship helix
+sudo pacman -S nautilus firefox fuzzel hyprlock starship helix awww
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+# Create the directory if it doesn't exist
+sudo mkdir -p /etc/systemd/resolved.conf.d/
+
+# Write the DNS rules using echo
+echo -e "[Resolve]\nDNS=185.228.168.168 185.228.169.168\nDNSOverTLS=no" | sudo tee /etc/systemd/resolved.conf.d/cleanbrowsing.conf > /dev/null
+
+# Restart the service
+sudo systemctl restart systemd-resolved
 
 echo "rebooting"
 
